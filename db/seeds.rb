4c507484222123c830f03f86cb37222b0e7eaa01
Adriana Vars <asvutah@gmail.com>
2019-01-10 21:57:08 -0700

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-100.times do
+20.times do
   Product.create(
     name: Faker::Commerce.product_name,
     price: Faker::Commerce.price.to_f,
     description: Faker::Commerce.material,
-    stock: Faker::Lorem.sentence,
     stock: 1 + rand(10),
     image: Faker::Avatar.image
   )
