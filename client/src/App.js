2af32e1899dbccc71eaa5b2ecdd723b48d1e186a
Adriana Vars <asvutah@gmail.com>
2019-01-16 23:11:34 -0700

    product page complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch, } from 'react-router-dom';
 import { Container, } from 'semantic-ui-react'
 import Products from './components/adminView/Products'
 import NoMatch from './components/NoMatch';
+import Product from './components/customerView/Product';
 
 const App = () => (
   <Fragment>
     <Container>
       <Switch>
         <Route exact path='/admin/products' component={Products} />
+        <Route exact path='/products/:id' component={Product} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
