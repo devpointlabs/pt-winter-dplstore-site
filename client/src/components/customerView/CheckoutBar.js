fd994ed07f07f974aa5ae6e7f68bf4ccfc3d7e8c
Henry Doan <henrydoan96@gmail.com>
2019-01-22 21:10:46 -0700

    Update CheckoutBar.js

--- client/src/components/customerView/CheckoutBar.js
+++ client/src/components/customerView/CheckoutBar.js
@@ -6,32 +6,31 @@ class CheckoutBar extends Component{
   render(){
       const { values } = this.props
       return(
-    <Segment>
-        <Step.Group ordered widths="equal">
-          <Step completed={this.props.status}>
-            <Step.Content>
-              <Step.Title>Order details</Step.Title>
-              <Step.Description>Enter your personal details</Step.Description>
-            </Step.Content>
-          </Step>
+        <Segment>
+          <Step.Group ordered widths="equal">
+            <Step completed={this.props.status}>
+              <Step.Content>
+                <Step.Title>Order details</Step.Title>
+                <Step.Description>Enter your personal details</Step.Description>
+               </Step.Content>
+             </Step>
 
-          <Step completed={this.props.status}>
-            <Step.Content>
-              <Step.Title>Confirm Personal Details </Step.Title>
-            </Step.Content>
-          </Step>
-        
-          <Step >
-          <Icon name='credit card' />
-            <Step.Content>
-              <Step.Title>Payment</Step.Title>
-              <Step.Description>Enter credit card information</Step.Description>
-            </Step.Content>
-          </Step>
+             <Step completed={this.props.status}>
+              <Step.Content>
+                <Step.Title>Confirm Personal Details </Step.Title>
+              </Step.Content>
+             </Step>
 
-        </Step.Group>
-    </Segment>
-      )
+             <Step >
+              <Icon name='credit card' />
+                <Step.Content>
+                  <Step.Title>Payment</Step.Title>
+                  <Step.Description>Enter credit card information</Step.Description>
+                </Step.Content>
+              </Step>
+          </Step.Group>
+        </Segment>
+    )
   }
 }
 
